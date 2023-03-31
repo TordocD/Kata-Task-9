cac4ded721179ca77a32d772e068c6e6b1529fc3
Dmitry Nedelko <dmitry30reg@mail.ru>
2023-03-31 21:05:17 +0400

    User created

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,60 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userName;
+    private String surname;
+    private int age;
+
+    public User() {
+
+    }
+
+    public User(String userName, String surname, int age) {
+        this.userName = userName;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
