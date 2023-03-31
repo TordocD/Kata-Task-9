ed0f904fcf0a6a23efbd9f073c686080aab45b83
Dmitry Nedelko <dmitry30reg@mail.ru>
2023-03-31 21:44:04 +0400

    UserService created

---
+++ src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,66 @@
+package service;
+
+import dao.UserDao;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void createTable() {
+        userDao.createTable();
+    }
+
+    @Override
+    public void dropTable() {
+        userDao.dropTable();
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void delete(User user) {
+        userDao.delete(user);
+    }
+
+    @Override
+    @Transactional
+    public void setName(User user, String newName) {
+        userDao.setName(user, newName);
+    }
+
+    @Override
+    @Transactional
+    public void setSurname(User user, String newSurname) {
+        userDao.setSurname(user, newSurname);
+    }
+
+    @Override
+    @Transactional
+    public void setAge(User user, int newAge) {
+        userDao.setAge(user, newAge);
+    }
+
+    @Override
+    @Transactional
+    public void setUser(User currentUser, User newUser) {
+        userDao.setUser(currentUser, newUser);
+    }
+}
